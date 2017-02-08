75d830ed371e39605b2d2409e2eb75f85b28fce4
Serorin <b5239@oic.jp>
2017-02-09 03:00:34 +0900

    念の為

--- resources/views/Top.blade.php
+++ resources/views/Top.blade.php
@@ -9,11 +9,22 @@
 
 <body>
 
+    <ul class="navi">
+      <li class="list">
+       <a href="/About"><img class="size" src="/img/6.png"></a>
+       <a href="/Work"><img class="size" src="/img/6.png"></a>
+       <a href="/Contact"><img class="size" src="/img/6.png"></a>
+      </li>
+    </ul>
+
     <div class="img">
-     <div class="img_1"></div>
-     <div class="img_2"></div>
-   </div>
-   
+      <div class="img_1"></div>
+    </div>
+
+
+   <h1>ようこそ</h1>
+   <p>ここはせろりのポートフォリオサイトです</p>
+
 </body>
 
 </html>
